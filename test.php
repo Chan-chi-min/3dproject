3aacf547850fd0bf20097c1406a336f2ddc39a84
chan-chi-min <qimin.jeff@gmail.com>
2020-06-02 23:48:19 +0800

    321

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+ 
+// Cross-Origin Resource Sharing Header
+header('Access-Control-Allow-Origin: new%202.html');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+ 
+?>
